d92e12dd82bb17cfe9d48d826faeae8aaef6c548
Elli6070 <55157114+Elli6070@users.noreply.github.com>
2020-07-29 00:52:59 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var app = require("express")();
 var http = require("http").createServer(app);
 var io = require ("socket.io")(http);
 
+var port = process.env.PORT || 8000;
+
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/calculator.html");
 });
@@ -16,7 +18,7 @@ io.on("connection", (socket) => {
 	});
 });
 
-http.listen(8000, () => {
-	console.log("listening on 8000");
+http.listen(port, () => {
+	console.log("listening on heroku port");
 });
 
